afafc57893f5906edbcbce4f35e694e86c61447c
Carlos Rodriguez <carlos@eddorre.com>
2013-04-06 15:43:47 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hydra::Application.config.secret_token = 'e95ab11778bd7ed82d611fdff74317d9ef6138ed2a417a9d07bee1aa3906f4a2360b96c8d614752f2415321858d33222925f03169180cd7200bf626b7e6b60bf'
