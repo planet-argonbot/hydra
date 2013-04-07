376ef33be09b452adff56a3dc30445b401e7491e
Carlos Rodriguez <carlos@eddorre.com>
2013-04-07 10:49:21 -0700

    Added projects features.

---
+++ spec/features/projects_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature "Viewing a list of projects" do
+  background do
+    setup_admin_and_sign_in
+  end
+
+  scenario "An admin should be able to see a list of projects" do
+    create(:project, name: 'This is a test project')
+
+    visit admin_projects_path
+
+    expect(page).to have_content 'This is a test project'
+  end
+end
