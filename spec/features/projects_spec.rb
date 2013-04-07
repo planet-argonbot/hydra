edea11a5183878b96cd8f3c51bf4e839b0a68bd4
Carlos Rodriguez <carlos@eddorre.com>
2013-04-07 16:17:21 -0700

    Added pending scenario for projects.

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -13,3 +13,13 @@ feature "Viewing a list of projects" do
     expect(page).to have_content 'This is a test project'
   end
 end
+
+feature "Viewing an individual project" do
+  background do
+    setup_admin_and_sign_in
+  end
+
+  scenario "An admin should be able to see the project name"
+
+  scenario "An admin should be able to see releases for the project"
+end
