5eaa9c4a755c36a9744ad56f8f47a396f3a95680
Carlos Rodriguez <carlos@eddorre.com>
2013-04-07 10:51:28 -0700

    Added helper method setup_admin_and_sign_in

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -4,6 +4,15 @@ module HelperMethods
 
     @admin = create(:admin, password: password, password_confirmation: password)
   end
+
+  def setup_admin_and_sign_in
+    setup_admin
+
+    visit new_admin_session_path
+    fill_in :admin_email, :with => @admin.email
+    fill_in :admin_password, :with => 'test123456'
+    click_button "Sign in"
+  end
 end
 
 RSpec.configuration.include HelperMethods
