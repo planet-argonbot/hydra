689a1e1b90e579df56c4b01f6e7a85c5a82f7ea3
Carlos Rodriguez <carlos@eddorre.com>
2013-04-07 21:42:46 -0700

    Added model comments, name method and associations to releases.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,11 +1,25 @@
 class Admin < ActiveRecord::Base
+  # Associations
+  has_many :releases
+
+  # Validations
+
+  # Scopes
+
+  # Extensions
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  # Callbacks
+
+  # Attributes
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
