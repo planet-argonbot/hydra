57eb20e266bf32c33ef8f1042e241e95ec944210
Carlos Rodriguez <carlos@eddorre.com>
2013-04-07 21:44:31 -0700

    Added first_name, last_name, and password to admin factory.

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -2,6 +2,9 @@
 
 FactoryGirl.define do
   factory :admin do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
+    password { Faker::Name.name }
   end
 end
