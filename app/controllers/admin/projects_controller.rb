193efcb654749da297525a6c33c9ef7a14340b95
Carlos Rodriguez <carlos@eddorre.com>
2013-04-07 21:02:29 -0700

    Added before_filter, fetch_project and protected methods.

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,5 +1,12 @@
 class Admin::ProjectsController < Admin::ApplicationController
+  before_filter :fetch_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
+  end
+
+  protected
+  def fetch_project
+    @project = Project.find params[:id]
   end
 end
