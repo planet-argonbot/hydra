b952021ff860bd9238b352d90271c38fd00169a0
Carlos Rodriguez <carlos@eddorre.com>
2013-04-08 22:33:17 -0700

    Added create action for api/releases.

--- app/controllers/api/releases_controller.rb
+++ app/controllers/api/releases_controller.rb
@@ -1,2 +1,32 @@
-class Api::ReleasesController < ApplicationController
+class Api::ReleasesController < Api::BaseController
+  before_filter :fetch_project
+
+  respond_to :json
+
+  def create
+    respond_with('No JSON payload specified', status: :precondition_failed, location: api_project_releases_path) and return if params[:release].blank?
+
+    parsed_payload = JSON.parse(params[:release]).symbolize_keys!
+
+    @release = @project.releases.build(parsed_payload)
+
+    if @release.save
+      respond_with(@release, status: :ok, location: api_project_releases_path)
+    else
+      respond_with('Could not create release', status: :internal_server_error, location: api_project_releases_path)
+    end
+
+    rescue JSON::ParserError
+      respond_with('Payload is not in JSON format', status: :precondition_failed, location: api_project_releases_path)
+  end
+
+  protected
+  def fetch_project
+    @project = Project.find_by_name params[:project_id]
+
+    raise ActiveRecord::RecordNotFound if @project.blank?
+
+    rescue ActiveRecord::RecordNotFound
+      respond_with('Project not found', status: :not_found, location: api_project_releases_path)
+  end
 end
