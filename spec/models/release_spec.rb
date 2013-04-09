b69dc03c99563034a018b49c47c8065a50b3d675
Carlos Rodriguez <carlos@eddorre.com>
2013-04-08 23:02:49 -0700

    Added specs for a release.

--- spec/models/release_spec.rb
+++ spec/models/release_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Release do
+  let(:release) { build(:release) }
 
+  it "should not be valid without a project" do
+    release.project = nil
+
+    expect(release).not_to be_valid
+    expect(release).to have(1).error_on(:project)
+  end
+
+  it "should not be valid without an admin" do
+    release.admin = nil
+
+    expect(release).not_to be_valid
+    expect(release).to have(1).error_on(:admin)
+  end
 end
