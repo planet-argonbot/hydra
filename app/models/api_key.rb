ba97df3ae2fce5d21715d6c9865bbafdbafdf828
Carlos Rodriguez <carlos@eddorre.com>
2013-04-08 21:18:31 -0700

    Added ApiKey model, spec, migration and factory.

---
+++ app/models/api_key.rb
@@ -0,0 +1,40 @@
+class ApiKey < ActiveRecord::Base
+  # Associations
+
+  # Delegates
+
+  # Validations
+  validates :access_token, uniqueness: true
+
+  # Scopes
+
+  # Extensions
+  include AASM
+
+  # Callbacks
+  before_create :generate_access_token
+
+  # Attributes
+  attr_accessible :access_token
+
+  # States
+  aasm do
+    state :active, initial: true
+    state :revoked
+
+    event :revoke do
+      transitions from: :active, to: :revoked
+    end
+
+    event :activate do
+      transitions from: :revoked, to: :active
+    end
+  end
+
+  private
+  def generate_access_token
+    begin
+      self.access_token = SecureRandom.hex
+      end while self.class.exists?(access_token: access_token)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130408043507) do
+ActiveRecord::Schema.define(:version => 20130409035719) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(:version => 20130408043507) do
 
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
   add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
+
+  create_table "api_keys", :force => true do |t|
+    t.string   "access_token"
+    t.string   "aasm_state"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "projects", :force => true do |t|
     t.string   "name"
