1554770423e0eed091999494a7c120a2eca5dad4
Carlos Rodriguez <carlos@eddorre.com>
2013-04-08 23:02:27 -0700

    Added validation for project and admin. Added private method, set_admin that is triggered after create. Added email_address as an attr_accessor and attr_accessible.

--- app/models/release.rb
+++ app/models/release.rb
@@ -4,13 +4,21 @@ class Release < ActiveRecord::Base
   belongs_to :project
 
   # Validations
+  validates :project, :admin, presence: true
 
   # Scopes
 
   # Extensions
 
   # Callbacks
+  before_validation :set_admin, on: :create
 
   # Attributes
-  attr_accessible :branch, :environment, :username
+  attr_accessible :branch, :environment, :username, :email_address
+  attr_accessor :email_address
+
+  private
+  def set_admin
+    self.admin = Admin.where("email = ?", self.email_address).first
+  end
 end
