878f3fa3cd482837e6bf9685cc300497e26d5de6
Carlos Rodriguez <carlos@eddorre.com>
2013-05-04 13:49:31 -0700

    Added private method, fetch_api_key. Added actions for create and for update.

--- app/controllers/admin/api_keys_controller.rb
+++ app/controllers/admin/api_keys_controller.rb
@@ -1,5 +1,30 @@
 class Admin::ApiKeysController < ApplicationController
+  before_filter :fetch_api_key, only: [ :edit, :update ]
+
   def index
     @api_keys = ApiKey.all
+  end
+
+  def create
+    ApiKey.create
+    add_message "An API key was successfully generated."
+    redirect_to admin_api_keys_path
+  end
+
+  def update
+    state_change = params.fetch(:status_change, 'activate')
+
+    @api_key.revoke! if state_change == 'revoke' && @api_key.active?
+
+    @api_key.activate! if state_change == 'activate' && @api_key.revoked?
+
+    add_message "Changed status for API Key #{@api_key.access_token}"
+
+    redirect_to admin_api_keys_path
+  end
+
+  private
+  def fetch_api_key
+    @api_key = ApiKey.where("access_token = ?", params[:id]).first
   end
 end
