46d2df210fa7d066ba0a8431129a16c6263083e7
Carlos Rodriguez <carlos@eddorre.com>
2013-05-04 13:55:50 -0700

    Added more specs for API keys.

--- spec/features/api_keys_spec.rb
+++ spec/features/api_keys_spec.rb
@@ -12,4 +12,28 @@ feature "Viewing a list of API keys" do
 
     expect(page).to have_content api_key.access_token
   end
+
+  scenario "An admin should see a message that there are no API keys when none have been created" do
+    visit admin_api_keys_path
+
+    expect(page).to have_content "There are no API keys."
+  end
+
+  scenario "An admin should be able to generate an API key when none have been created" do
+    visit admin_api_keys_path
+
+    click_link "Generate an API key"
+
+    expect(page).not_to have_content "There are no API keys."
+  end
+
+  scenario "An admin should be able to revoke an API key" do
+    api_key = create(:api_key)
+
+    visit admin_api_keys_path
+
+    click_link 'revoke'
+
+    expect(page).to have_content "revoked"
+  end
 end
