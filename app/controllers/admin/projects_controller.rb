24cd769a8b5f47931e8b4a9ff55590347066465d
Carlos Rodriguez <carlos@eddorre.com>
2013-05-03 23:07:22 -0700

    Added feature to allow creation of projects.

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -9,6 +9,22 @@ class Admin::ProjectsController < Admin::ApplicationController
     @releases = @project.releases.order("deployed_at DESC")
   end
 
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(params[:project])
+
+    if @project.save
+      add_message "Project was created successfully."
+      redirect_to admin_projects_path
+    else
+      add_error "There was an error creating the project. Please try again."
+      render :new
+    end
+  end
+
   protected
   def fetch_project
     @project = Project.where("name = ?", params[:id]).first
--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -42,3 +42,20 @@ feature "Viewing an individual project" do
     expect(page).to have_content release.branch
   end
 end
+
+feature "Creating a new project" do
+  background do
+    setup_admin_and_sign_in
+  end
+
+  scenario "An admin should be able to create a new project" do
+    visit new_admin_project_path
+
+    name = Faker::Name.name
+
+    fill_in :project_name, with: name
+    click_button "Create project"
+
+    expect(page).to have_content name
+  end
+end
