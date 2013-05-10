f2a4cb023683ce5c2ed34455a1044741a7f0d7ea
Carlos Rodriguez <carlos@eddorre.com>
2013-05-09 21:50:05 -0700

    Added admin/releases controller and generated files.

---
+++ app/controllers/admin/releases_controller.rb
@@ -0,0 +1,30 @@
+class Admin::ReleasesController < ApplicationController
+  before_filter :fetch_project
+
+  respond_to :html
+
+  def new
+    @release = @project.releases.new
+  end
+
+  def create
+    @release = @project.releases.new(params[:release])
+
+    respond_with(@release) do |format|
+      if @release.save
+        add_message "Release was created successfully"
+        format.html { redirect_to admin_project_path(@project) }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  protected
+  def fetch_project
+    @project = Project.find(params[:project_id])
+
+    rescue ActiveRecord::RecordNotFound
+      respond_with('Project not found', status: :not_found, location: api_project_releases_path)
+  end
+end
