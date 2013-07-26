8781d937b88fcfcb47faff99db872c4ef0856d9d
Carlos Rodriguez <carlos@eddorre.com>
2013-07-26 12:53:14 -0700

    Added attr_accessor save_context. Added method generate random password that gets called in a before_validation filter. A random password will be generated if the the save_context is equal to :ui.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -13,13 +13,18 @@ class Admin < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   # Callbacks
+  before_validation :generate_random_password
 
   # Attributes
-
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
+  attr_accessor :save_context
 
   def name
     "#{self.first_name} #{self.last_name}"
+  end
+
+  protected
+  def generate_random_password
+    self.password = self.password_confirmation = SecureRandom.base64(13) if self.save_context == :ui
   end
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe Admin do
+  it "should automatically generate an random password if the save content is ui." do
+    admin = build(:admin, password: nil)
+    admin.save_context = :ui
+    admin.save
+
+    expect(admin.encrypted_password).not_to be_blank
+  end
 end
