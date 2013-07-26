596daaf8f97da685454e9243a9e16fe54912f43a
Carlos Rodriguez <carlos@eddorre.com>
2013-07-26 11:07:57 -0700

    Added admins show spec.

--- spec/features/admins_spec.rb
+++ spec/features/admins_spec.rb
@@ -7,9 +7,13 @@ feature "An admin viewing other admins" do
 
   scenario "An admin should be able to see a list of all admins" do
     admin = create(:admin)
-
     visit admin_admins_path
+    expect(page).to have_content admin.name
+  end
 
+  scenario "An admin should be able to view an admin" do
+    admin = create(:admin)
+    visit admin_admin_path(admin)
     expect(page).to have_content admin.name
   end
 end
