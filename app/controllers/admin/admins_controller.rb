3981e9f36a017c0bfe68b0233085b72b3b44cb92
Carl Anderson <carl@planetargon.com>
2013-07-26 13:24:36 -0700

    Added new method to create admins.

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -8,4 +8,20 @@ class Admin::AdminsController < Admin::ApplicationController
     @admin = Admin.where(id: params[:id]).first
   end
 
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params[:admin])
+
+    if @admin.save
+      add_message "Admin was created successfully."
+      redirect_to admin_admins_path
+    else
+      add_error "There was an error creating the admin. Please try again."
+      render :new
+    end
+  end
+
 end
